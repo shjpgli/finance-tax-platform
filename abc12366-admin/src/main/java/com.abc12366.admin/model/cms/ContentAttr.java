553afc45f89844aca309611363b0e8194cf9b50a
xieyanmao <xieyanmao@dingtalk.com>
2017-04-25 15:03:21 +0800

    Squashed commit of the following:
    commit 77ef0f34f3e4d767dc475a855ebe572182d77924
    Author: xieyanmao <xieyanmao@dingtalk.com>
    Date:   Tue Apr 25 14:50:33 2017 +0800
    
        新增内容模块类

---
+++ abc12366-admin/src/main/java/com.abc12366.admin/model/cms/ContentAttr.java
@@ -0,0 +1,49 @@
+package com.abc12366.admin.model.cms;
+import java.io.Serializable;
+
+
+/**
+ *
+ * CMS内容扩展属性表
+ * add by xieyanmao on 2017-4-25
+ *
+ **/
+@SuppressWarnings("serial")
+public class ContentAttr implements Serializable {
+
+	/****/
+	private String contentId;
+
+	/**名称**/
+	private String attrName;
+
+	/**值**/
+	private String attrValue;
+
+
+
+	public void setContentId(String contentId){
+		this.contentId = contentId;
+	}
+
+	public String getContentId(){
+		return this.contentId;
+	}
+
+	public void setAttrName(String attrName){
+		this.attrName = attrName;
+	}
+
+	public String getAttrName(){
+		return this.attrName;
+	}
+
+	public void setAttrValue(String attrValue){
+		this.attrValue = attrValue;
+	}
+
+	public String getAttrValue(){
+		return this.attrValue;
+	}
+
+}
