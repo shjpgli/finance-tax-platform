e3f337628999a90a2f3681457dc8249bf7ce623b
lizhongwei <MY@DESKTOP-G1571LD>
2017-04-25 15:15:19 +0800

    cms 栏目和栏目模型，site等model文件提交

---
+++ abc12366-cms/src/main/java/com/abc12366/cms/mapper/FriendlinkMapper.java
@@ -0,0 +1,58 @@
+package com.abc12366.cms.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.abc12366.cms.model.Friendlink;
+
+/**
+ * 
+ * FriendlinkMapper数据库操作接口类
+ * 
+ **/
+
+public interface FriendlinkMapper{
+
+
+	/**
+	 * 
+	 * 查询（根据主键ID查询）
+	 * 
+	 **/
+	Friendlink  selectByPrimaryKey ( @Param("id") Long id );
+
+	/**
+	 * 
+	 * 删除（根据主键ID删除）
+	 * 
+	 **/
+	int deleteByPrimaryKey ( @Param("id") Long id );
+
+	/**
+	 * 
+	 * 添加
+	 * 
+	 **/
+	int insert( Friendlink record );
+
+	/**
+	 * 
+	 * 添加 （匹配有值的字段）
+	 * 
+	 **/
+	int insertSelective( Friendlink record );
+
+	/**
+	 * 
+	 * 修改 （匹配有值的字段）
+	 * 
+	 **/
+	int updateByPrimaryKeySelective( Friendlink record );
+
+	/**
+	 * 
+	 * 修改（根据主键ID修改）
+	 * 
+	 **/
+	int updateByPrimaryKey ( Friendlink record );
+
+}
