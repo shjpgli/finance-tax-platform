b87b212ae4a9b9521a6e5f9c4dc7255bb8bb3462
xieyanmao <maozi123>
2017-06-19 18:18:54 +0800

    增加网关

--- abc12366-cms/src/main/java/com/abc12366/cms/config/DatabaseConfig.java
+++ abc12366-cms/src/main/java/com/abc12366/cms/config/DatabaseConfig.java
@@ -181,4 +181,135 @@ public class DatabaseConfig implements EnvironmentAware {
     public PlatformTransactionManager db2TxManager() throws SQLException {
         return new DataSourceTransactionManager(db2DataSource());
     }
+
+    @Bean(name = "gw1DataSource", initMethod = "init", destroyMethod = "close")
+    public DruidDataSource gw1DataSource() throws SQLException {
+        // druid数据源
+        if (StringUtils.isEmpty(propertyResolver.getProperty("gw1.url"))) {
+            System.out.println("Your database connection pool configuration is incorrect!" +
+                    " Please check your Spring profile, current profiles are:" +
+                    Arrays.toString(environment.getActiveProfiles()));
+            throw new ApplicationContextException(
+                    "Database connection pool is not configured correctly");
+        }
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(propertyResolver.getProperty("gw1.driverClassName"));
+        druidDataSource.setUrl(propertyResolver.getProperty("gw1.url"));
+        druidDataSource.setUsername(propertyResolver.getProperty("gw1.username"));
+        druidDataSource.setPassword(propertyResolver.getProperty("gw1.password"));
+        druidDataSource.setInitialSize(Integer.parseInt(propertyResolver.getProperty("gw1.initialSize")));
+        druidDataSource.setMinIdle(Integer.parseInt(propertyResolver.getProperty("gw1.minIdle")));
+        druidDataSource.setMaxActive(Integer.parseInt(propertyResolver.getProperty("gw1.maxActive")));
+        druidDataSource.setMaxWait(Integer.parseInt(propertyResolver.getProperty("gw1.maxWait")));
+        druidDataSource.setTimeBetweenEvictionRunsMillis(Long.parseLong(propertyResolver.getProperty("gw1.timeBetweenEvictionRunsMillis")));
+        druidDataSource.setMinEvictableIdleTimeMillis(Long.parseLong(propertyResolver.getProperty("gw1.minEvictableIdleTimeMillis")));
+        druidDataSource.setValidationQuery(propertyResolver.getProperty("gw1.validationQuery"));
+        druidDataSource.setTestWhileIdle(Boolean.parseBoolean(propertyResolver.getProperty("gw1.testWhileIdle")));
+        druidDataSource.setTestOnBorrow(Boolean.parseBoolean(propertyResolver.getProperty("gw1.testOnBorrow")));
+        druidDataSource.setTestOnReturn(Boolean.parseBoolean(propertyResolver.getProperty("gw1.testOnReturn")));
+        druidDataSource.setPoolPreparedStatements(Boolean.parseBoolean(propertyResolver.getProperty("gw1.poolPreparedStatements")));
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(Integer.parseInt(propertyResolver.getProperty("gw1.maxPoolPreparedStatementPerConnectionSize")));
+        druidDataSource.setFilters(propertyResolver.getProperty("gw1.filters"));
+        druidDataSource.setUseGlobalDataSourceStat(Boolean.parseBoolean(propertyResolver.getProperty("gw1.useGlobalDataSourceStat")));
+        return druidDataSource;
+    }
+
+    @Bean(name = "gw2DataSource", initMethod = "init", destroyMethod = "close")
+    public DataSource gw2DataSource() throws SQLException {
+        // druid数据源
+        if (StringUtils.isEmpty(propertyResolver.getProperty("gw2.url"))) {
+            System.out.println("Your database connection pool configuration is incorrect!" +
+                    " Please check your Spring profile, current profiles are:" +
+                    Arrays.toString(environment.getActiveProfiles()));
+            throw new ApplicationContextException(
+                    "Database connection pool is not configured correctly");
+        }
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(propertyResolver.getProperty("gw2.driverClassName"));
+        druidDataSource.setUrl(propertyResolver.getProperty("gw2.url"));
+        druidDataSource.setUsername(propertyResolver.getProperty("gw2.username"));
+        druidDataSource.setPassword(propertyResolver.getProperty("gw2.password"));
+        druidDataSource.setInitialSize(Integer.parseInt(propertyResolver.getProperty("gw2.initialSize")));
+        druidDataSource.setMinIdle(Integer.parseInt(propertyResolver.getProperty("gw2.minIdle")));
+        druidDataSource.setMaxActive(Integer.parseInt(propertyResolver.getProperty("gw2.maxActive")));
+        druidDataSource.setMaxWait(Integer.parseInt(propertyResolver.getProperty("gw2.maxWait")));
+        druidDataSource.setTimeBetweenEvictionRunsMillis(Long.parseLong(propertyResolver.getProperty("gw2.timeBetweenEvictionRunsMillis")));
+        druidDataSource.setMinEvictableIdleTimeMillis(Long.parseLong(propertyResolver.getProperty("gw2.minEvictableIdleTimeMillis")));
+        druidDataSource.setValidationQuery(propertyResolver.getProperty("gw2.validationQuery"));
+        druidDataSource.setTestWhileIdle(Boolean.parseBoolean(propertyResolver.getProperty("gw2.testWhileIdle")));
+        druidDataSource.setTestOnBorrow(Boolean.parseBoolean(propertyResolver.getProperty("gw2.testOnBorrow")));
+        druidDataSource.setTestOnReturn(Boolean.parseBoolean(propertyResolver.getProperty("gw2.testOnReturn")));
+        druidDataSource.setPoolPreparedStatements(Boolean.parseBoolean(propertyResolver.getProperty("gw2.poolPreparedStatements")));
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(Integer.parseInt(propertyResolver.getProperty("gw2.maxPoolPreparedStatementPerConnectionSize")));
+        druidDataSource.setFilters(propertyResolver.getProperty("gw2.filters"));
+        druidDataSource.setUseGlobalDataSourceStat(Boolean.parseBoolean(propertyResolver.getProperty("gw2.useGlobalDataSourceStat")));
+        return druidDataSource;
+    }
+
+    @Bean("gw1SqlSessionFactory")
+    @Primary
+    public SqlSessionFactory gw1SqlSessionFactory() throws SQLException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        // 设置数据源
+        sqlSessionFactoryBean.setDataSource(gw1DataSource());
+        // 自动重命名
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.abc12366.gateway.model");
+        // 设置 typeHandler
+//        sqlSessionFactoryBean.setTypeHandlersPackage("com.example.project.typeHandler");
+        // 添加拦截器插件 (如果有的话, 分页插件, 分表插件等)
+//        sqlSessionFactory.setPlugins(new Interceptor[]{pageHelper});
+        // 设置 mapper 文件
+        try {
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            sqlSessionFactoryBean.setMapperLocations(resolver
+                    .getResources("classpath:com/abc12366/gateway/mapper/db1/*.xml"));
+            return sqlSessionFactoryBean.getObject();
+        } catch (Exception e) {
+            LOGGER.error("初始化GW1SqlSessionFactory失败", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean("gw2SqlSessionFactory")
+    public SqlSessionFactory gw2SqlSessionFactory() throws SQLException {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        // 设置数据源
+        sqlSessionFactoryBean.setDataSource(gw2DataSource());
+        // 自动重命名
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.abc12366.gateway.model");
+        // 设置 typeHandler
+//        sqlSessionFactoryBean.setTypeHandlersPackage("com.example.project.typeHandler");
+        // 添加拦截器插件 (如果有的话, 分页插件, 分表插件等)
+//        sqlSessionFactory.setPlugins(new Interceptor[]{pageHelper});
+        // 设置 mapper 文件
+        try {
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+            sqlSessionFactoryBean.setMapperLocations(resolver
+                    .getResources("classpath:com/abc12366/gateway/mapper/db2/*.xml"));
+            return sqlSessionFactoryBean.getObject();
+        } catch (Exception e) {
+            LOGGER.error("初始化DB2SqlSessionFactory失败", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean("gw1SqlSessionTemplate")
+    public SqlSessionTemplate gw1SqlSessionTemplate() throws SQLException {
+        return new SqlSessionTemplate(gw1SqlSessionFactory());
+    }
+
+    @Bean("gw2SqlSessionTemplate")
+    public SqlSessionTemplate gw2SqlSessionTemplate() throws SQLException {
+        return new SqlSessionTemplate(gw2SqlSessionFactory());
+    }
+
+    @Bean("gw1TxManager")
+    public PlatformTransactionManager gw1TxManager() throws SQLException {
+        return new DataSourceTransactionManager(gw1DataSource());
+    }
+
+    @Bean("gw2TxManager")
+    public PlatformTransactionManager gw2TxManager() throws SQLException {
+        return new DataSourceTransactionManager(gw2DataSource());
+    }
 }
