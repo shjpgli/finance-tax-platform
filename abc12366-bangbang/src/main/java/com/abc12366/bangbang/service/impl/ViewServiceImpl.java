c33e10c94c96ce51e2ecec206a424c5411d4dd50
liuguiyao <435720953@qq.com>
2017-07-12 10:34:45 +0800

    帮帮新增浏览历史接口

---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/impl/ViewServiceImpl.java
@@ -0,0 +1,84 @@
+package com.abc12366.bangbang.service.impl;
+
+import com.abc12366.bangbang.common.UcUserCommon;
+import com.abc12366.bangbang.mapper.db1.ViewMapper;
+import com.abc12366.bangbang.mapper.db2.ViewRoMapper;
+import com.abc12366.bangbang.model.View;
+import com.abc12366.bangbang.model.bo.ViewBO;
+import com.abc12366.bangbang.service.ViewService;
+import com.abc12366.common.exception.ServiceException;
+import com.abc12366.common.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * User: liuguiyao<435720953@qq.com>
+ * Date: 2017-07-12
+ * Time: 9:34
+ */
+@Service
+public class ViewServiceImpl implements ViewService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ViewServiceImpl.class);
+
+    @Autowired
+    private ViewMapper viewMapper;
+
+    @Autowired
+    private ViewRoMapper viewRoMapper;
+
+    @Override
+    public ViewBO insert(String askId, HttpServletRequest request) {
+        LOGGER.info("{}:{}", askId, request);
+        String userId = UcUserCommon.getUserId(request);
+        Date date = new Date();
+        View view = new View();
+        view.setId(Utils.uuid());
+        view.setAskId(askId);
+        view.setUserId(userId);
+        view.setStatus(true);
+        view.setCreateTime(date);
+        view.setLastUpdate(date);
+        int result = viewMapper.insert(view);
+        if (result < 1) {
+            throw new ServiceException(4710);
+        }
+        ViewBO viewBO = new ViewBO();
+        BeanUtils.copyProperties(view, viewBO);
+        return viewBO;
+    }
+
+    @Override
+    public void delete(String askId, HttpServletRequest request) {
+        LOGGER.info("{}:{}", askId, request);
+        String userId = UcUserCommon.getUserId(request);
+        View view = new View();
+        view.setAskId(askId);
+        view.setUserId(userId);
+        view.setStatus(false);
+        view.setLastUpdate(new Date());
+        int result = viewMapper.update(view);
+        if (result < 1) {
+            throw new ServiceException(4711);
+        }
+    }
+
+    @Override
+    public List<ViewBO> selectList(String userId) {
+        LOGGER.info("{}", userId);
+        return viewRoMapper.selectList(userId);
+    }
+
+    @Override
+    public int selectCount(String askId) {
+        LOGGER.info("{}", askId);
+        return Integer.parseInt(viewRoMapper.selectCount(askId));
+    }
+}
---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/web/ViewController.java
@@ -0,0 +1,64 @@
+package com.abc12366.bangbang.web;
+
+import com.abc12366.bangbang.model.bo.SupportBO;
+import com.abc12366.bangbang.model.bo.ViewBO;
+import com.abc12366.bangbang.service.ViewService;
+import com.abc12366.common.util.Constant;
+import com.abc12366.common.util.Utils;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * User: liuguiyao<435720953@qq.com>
+ * Date: 2017-07-11
+ * Time: 11:45
+ */
+@RestController
+@RequestMapping(headers = Constant.VERSION_HEAD + "=" + Constant.VERSION_1)
+public class ViewController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ViewController.class);
+
+    @Autowired
+    private ViewService viewService;
+
+    @PostMapping(path = "/view/{askId}")
+    public ResponseEntity insert(@PathVariable String askId, HttpServletRequest request) {
+        LOGGER.info("{}:{}",askId, request);
+        ViewBO viewBO = viewService.insert(askId, request);
+        return ResponseEntity.ok(Utils.kv("data", viewBO));
+    }
+
+    @DeleteMapping(path = "/view/{askId}")
+    public ResponseEntity delete(@PathVariable String askId, HttpServletRequest request) {
+        LOGGER.info("{}:{}", askId, request);
+        viewService.delete(askId, request);
+        return ResponseEntity.ok(Utils.kv());
+    }
+
+    @GetMapping(path = "/view/{userId}")
+    public ResponseEntity selectList(@PathVariable String userId,
+                                     @RequestParam(value = "page", defaultValue = Constant.pageNum) int page,
+                                     @RequestParam(value = "size", defaultValue = Constant.pageSize) int size) {
+        LOGGER.info("{}:{}:{}", userId, page, size);
+        PageHelper.startPage(page, size, true).pageSizeZero(true).reasonable(true);
+        List<ViewBO> viewBOList = viewService.selectList(userId);
+        return (viewBOList == null) ?
+                ResponseEntity.ok(Utils.kv()) :
+                ResponseEntity.ok(Utils.kv("dataList", (Page) viewBOList, "total", ((Page) viewBOList).getTotal()));
+    }
+    @GetMapping(path = "/count/view/{askId}")
+    public ResponseEntity selectCount(@PathVariable String askId) {
+        LOGGER.info("{}", askId);
+        int count = viewService.selectCount(askId);
+        return ResponseEntity.ok(Utils.kv("data", count));
+    }
+
+}
