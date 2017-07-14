01890d7c87483e05895af6ba5425f3456f40c347
xieyanmao <xieyanmao@dingtalk.com>
2017-07-14 10:08:17 +0800

    删除栏目时同时删除其下的统计信息

--- abc12366-cms/src/main/java/com/abc12366/cms/mapper/db1/ChannelCountMapper.java
+++ abc12366-cms/src/main/java/com/abc12366/cms/mapper/db1/ChannelCountMapper.java
@@ -17,7 +17,7 @@ public interface ChannelCountMapper{
 	 * 删除（根据主键ID删除）
 	 * 
 	 **/
-	int deleteByPrimaryKey(@Param("id") Long id);
+	int deleteByPrimaryKey(@Param("channelId") String channelId);
 
 	/**
 	 * 
