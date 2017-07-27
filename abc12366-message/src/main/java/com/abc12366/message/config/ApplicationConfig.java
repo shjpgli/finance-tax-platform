4cdbad48dfb9e490412692585e328de2303fd584
ljun51 <ljun51@outlook.com>
2017-07-27 11:00:00 +0800

    新增kafka配置

---
+++ abc12366-message/src/main/java/com/abc12366/message/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.abc12366.message.config;
+
+/**
+ * 应用配置文件
+ *
+ * @author lijun <ljun51@outlook.com>
+ * @create 2017-07-27 10:56 AM
+ * @since 1.0.0
+ */
+public class ApplicationConfig {
+
+    BOOTSTRAP_SERVERS;
+    CONSUMER_GROUP_ID
+}
