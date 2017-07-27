5cbf53b150e07b3a90e07372b77441fdbf699241
liuguiyao <435720953@qq.com>
2017-07-27 16:11:45 +0800

    帮帮新增热点问题、热点议论问题接口

---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/bo/HotspotAskBO.java
@@ -0,0 +1,122 @@
+package com.abc12366.bangbang.model.bo;
+
+import java.util.Date;
+
+/**
+ * User: liuguiyao<435720953@qq.com>
+ * Date: 2017-07-27
+ * Time: 10:43
+ */
+public class HotspotAskBO {
+    private String id;
+    private String userId;
+    private String ask;
+    private String detail;
+    private String status;
+    private Date createTime;
+    private Date lastUpdate;
+    private int points;
+    private Boolean isSolve;
+    private String askedUserId;
+    private String type;
+    private String pageviews;
+
+    public HotspotAskBO() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAsk() {
+        return ask;
+    }
+
+    public void setAsk(String ask) {
+        this.ask = ask;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public Boolean getIsSolve() {
+        return isSolve;
+    }
+
+    public void setIsSolve(Boolean isSolve) {
+        this.isSolve = isSolve;
+    }
+
+    public String getAskedUserId() {
+        return askedUserId;
+    }
+
+    public void setAskedUserId(String askedUserId) {
+        this.askedUserId = askedUserId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPageviews() {
+        return pageviews;
+    }
+
+    public void setPageviews(String pageviews) {
+        this.pageviews = pageviews;
+    }
+}
