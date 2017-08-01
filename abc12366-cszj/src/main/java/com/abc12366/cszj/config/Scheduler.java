a5642dc77987ef0f4038efcc8cba9137324f3322
zhushuai <xg214zs@163.com>
2017-08-01 21:12:00 +0800

    定时器优化

--- abc12366-cszj/src/main/java/com/abc12366/cszj/config/Scheduler.java
+++ abc12366-cszj/src/main/java/com/abc12366/cszj/config/Scheduler.java
@@ -3,10 +3,13 @@ package com.abc12366.cszj.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.abc12366.cszj.model.weixin.bo.WxUseToken;
+import com.abc12366.cszj.model.weixin.bo.gzh.GzhInfo;
+import com.abc12366.cszj.service.IWxGzhService;
 import com.abc12366.cszj.util.wx.WechatUrl;
 import com.abc12366.cszj.util.wx.WxConnectFactory;
 
@@ -18,19 +21,29 @@ import com.abc12366.cszj.util.wx.WxConnectFactory;
  */
 @Component
 public class Scheduler {
+	
+	@Autowired
+	private IWxGzhService iWxGzhService;
 
 	public static WxUseToken token = null;
 	// token获取失败，重复获取次数
 	private Integer regetTime = 10;
+	
+	private GzhInfo gzhInfo=null;
 
 	@Scheduled(fixedRate = 3600000)
 	public void getUserToken() {
+		
+		if(gzhInfo==null){
+			gzhInfo=iWxGzhService.wxgzhList(new GzhInfo(), 0, 1).get(0);
+		}
+		
 		int time = 0;
 		while (true) {
 			Map<String, String> tks = new HashMap<String, String>();
 			tks.put("grant_type", "client_credential");
-			tks.put("appid", SpringCtxHolder.getProperty("abc.wx-appid"));
-			tks.put("secret", SpringCtxHolder.getProperty("abc.wx-secret"));
+			tks.put("appid", gzhInfo.getAppid());
+			tks.put("secret", gzhInfo.getSecret());
 			token = WxConnectFactory.get(WechatUrl.WXUSETOKEN, tks, null,
 					WxUseToken.class);
 			if (0==token.getErrcode()) {
