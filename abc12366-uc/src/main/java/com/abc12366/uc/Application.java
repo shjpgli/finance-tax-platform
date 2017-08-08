df046e1d8bfd4b70bf9315947395c0f4827851d0
lsz <lsz@LAPTOP-ARC9APHI>
2017-08-08 10:16:38 +0800

    1

--- abc12366-uc/src/main/java/com/abc12366/uc/Application.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/Application.java
@@ -32,7 +32,7 @@ public class Application extends AsyncConfigurerSupport {
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(4);
         executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("uc-");
+        executor.setThreadNamePrefix("uc-");//uc-
         executor.initialize();
         return executor;
     }
