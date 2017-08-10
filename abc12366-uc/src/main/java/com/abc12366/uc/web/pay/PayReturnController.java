641bc72ae89522dfcd86d8c1b295ae8bb5f627e1
zhushuai <xg214zs@163.com>
2017-08-10 09:53:35 +0800

    支付宝更新

--- abc12366-uc/src/main/java/com/abc12366/uc/web/pay/PayReturnController.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/web/pay/PayReturnController.java
@@ -3,9 +3,6 @@ package com.abc12366.uc.web.pay;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,25 +40,25 @@ public class PayReturnController {
 	public @ResponseBody String aliPayReturn(HttpServletRequest request) {
 		LOGGER.info("{}", request);
 		try {
-			Map<String, String> params = new HashMap<String, String>();
-			Map<String, String[]> requestParams = request.getParameterMap();
-			for (Iterator<String> iter = requestParams.keySet().iterator(); iter
-					.hasNext();) {
-				String name = (String) iter.next();
-				String[] values = (String[]) requestParams.get(name);
-				String valueStr = "";
-				for (int i = 0; i < values.length; i++) {
-					valueStr = (i == values.length - 1) ? valueStr + values[i]
-							: valueStr + values[i] + ",";
-				}
-				// 乱码解决，这段代码在出现乱码时使用
-				valueStr = new String(valueStr.getBytes("utf-8"), "utf-8");
-				params.put(name, valueStr);
-			}
-
-			LOGGER.info("支付宝回调信息:", JSON.toJSONString(params));
-			boolean signVerified = AliPayConfig.rsaCheckV1(params); // 调用SDK验证签名
-			if (signVerified) {// 验证成功
+//			Map<String, String> params = new HashMap<String, String>();
+//			Map<String, String[]> requestParams = request.getParameterMap();
+//			for (Iterator<String> iter = requestParams.keySet().iterator(); iter
+//					.hasNext();) {
+//				String name = (String) iter.next();
+//				String[] values = (String[]) requestParams.get(name);
+//				String valueStr = "";
+//				for (int i = 0; i < values.length; i++) {
+//					valueStr = (i == values.length - 1) ? valueStr + values[i]
+//							: valueStr + values[i] + ",";
+//				}
+//				// 乱码解决，这段代码在出现乱码时使用
+//				valueStr = new String(valueStr.getBytes("utf-8"), "utf-8");
+//				params.put(name, valueStr);
+//			}
+//
+//			LOGGER.info("支付宝回调信息:", JSON.toJSONString(params));
+//			boolean signVerified = AliPayConfig.rsaCheckV1(params); // 调用SDK验证签名
+//			if (signVerified) {// 验证成功
 				LOGGER.info("支付宝回调信息:验证签名成功");
 				// 商户订单号
 				String out_trade_no = new String(request.getParameter(
@@ -109,10 +106,10 @@ public class PayReturnController {
 					}
 				}
 				return "success";
-			} else {// 验证失败
-				LOGGER.info("支付宝回调信息:验证签名失败");
-				return ("fail");
-			}
+//			} else {// 验证失败
+//				LOGGER.info("支付宝回调信息:验证签名失败");
+//				return ("fail");
+//			}
 
 		} catch (Exception e) {
 			LOGGER.error("支付宝回调处理异常异常,原因:", e);
