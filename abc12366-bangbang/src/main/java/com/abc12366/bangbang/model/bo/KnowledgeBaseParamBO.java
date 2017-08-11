3b7bc1b7582c3726e9f8bc613d310402f8c608d7
qiqiwill <liuqi281078208@163.com>
2017-08-11 14:58:44 +0800

    知识库增加 是否开发接口

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/bo/KnowledgeBaseParamBO.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/bo/KnowledgeBaseParamBO.java
@@ -12,10 +12,29 @@ public class KnowledgeBaseParamBO {
 
     private String keywords;    //关键字（知识库标题 模糊匹配）
 
-    public KnowledgeBaseParamBO(String categoryCode, String type, String keywords) {
+    private Boolean isOpen;     //是否对外开放
+
+    public KnowledgeBaseParamBO(String categoryCode, String type, String keywords, Boolean isOpen) {
         this.categoryCode = categoryCode;
         this.type = type;
         this.keywords = keywords;
+        this.isOpen = isOpen;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getIsOpen() {
+        return isOpen;
+    }
+
+    public void setIsOpen(Boolean isOpen) {
+        this.isOpen = isOpen;
     }
 
     public String getCategoryCode() {
