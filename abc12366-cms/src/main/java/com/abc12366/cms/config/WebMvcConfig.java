f91f39be3c6dc03e0c35b59dc12247bdc42f5e12
xieyanmao <maozi123>
2017-08-12 17:52:37 +0800

    新增推荐课程查询接口

--- abc12366-cms/src/main/java/com/abc12366/cms/config/WebMvcConfig.java
+++ abc12366-cms/src/main/java/com/abc12366/cms/config/WebMvcConfig.java
@@ -77,6 +77,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
                 .excludePathPatterns("/event/topone")
                         //根据访问量获取文章信息
                 .excludePathPatterns("/content/selectListByviews")
+                        //获取推荐课程
+                .excludePathPatterns("/content/selectRecommend")
                         // 财税专家客户端广告页、通知公告
                 .excludePathPatterns("/adpages", "adpages/**", "/notices", "/notices/**");
     }
