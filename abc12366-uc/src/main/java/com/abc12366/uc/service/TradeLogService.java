a6efd49d30ca203a2a437a6f1def41242c5cecf0
ljun51 <ljun51@outlook.com>
2017-08-12 16:05:03 +0800

    新作交易日志接口

--- abc12366-uc/src/main/java/com/abc12366/uc/service/TradeLogService.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/service/TradeLogService.java
@@ -1,6 +1,7 @@
 package com.abc12366.uc.service;
 
 import com.abc12366.uc.model.TradeLog;
+import com.abc12366.uc.model.bo.TradeBillBO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +11,12 @@ public interface TradeLogService {
 
     /**
      * 查询交易日志列表信息
-     *
-     * @param tradeLog
-     * @return
      */
-    List<TradeLog> selectList(TradeLog tradeLog);
+    List<TradeLog> selectList(TradeLog tradeLog, int page, int size);
     
     int insertTradeLog(TradeLog tradeLog);
 
+    List<TradeBillBO> bill(List<TradeBillBO> dataList);
+
+    TradeLog update(TradeLog log);
 }
--- abc12366-uc/src/main/java/com/abc12366/uc/web/TradeLogController.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/web/TradeLogController.java
@@ -3,21 +3,20 @@ package com.abc12366.uc.web;
 import com.abc12366.gateway.util.Constant;
 import com.abc12366.gateway.util.Utils;
 import com.abc12366.uc.model.TradeLog;
+import com.abc12366.uc.model.bo.TradeBillBO;
 import com.abc12366.uc.service.TradeLogService;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
- * 订单控制类
+ * 交易日志控制类
  *
  * @author lizhongwei
  * @create 2017-06-08
@@ -33,18 +32,69 @@ public class TradeLogController {
     private TradeLogService tradeLogService;
 
     /**
-     * 订单日志列表查询
+     * 交易日志列表查询
      */
-    @GetMapping(path = "/{orderNo}")
-    public ResponseEntity selectList(@PathVariable("orderNo") String orderNo) {
+    @GetMapping()
+    public ResponseEntity selectList(@RequestParam(value = "page", defaultValue = Constant.pageNum) int pageNum,
+                                     @RequestParam(value = "size", defaultValue = Constant.pageSize) int pageSize,
+                                     @PathVariable(value = "orderNo", required = false) String orderNo) {
+        LOGGER.info("{},{},{}", orderNo, pageNum, pageSize);
         TradeLog tradeLog = new TradeLog();
 
         tradeLog.setOrderNo(orderNo);
-        List<TradeLog> orderLogList = tradeLogService.selectList(tradeLog);
-        LOGGER.info("{}", orderLogList);
-        return (orderLogList == null) ?
-                new ResponseEntity<>(Utils.bodyStatus(4104), HttpStatus.BAD_REQUEST) :
-                ResponseEntity.ok(Utils.kv("orderLogList", orderLogList));
+        List<TradeLog> orderLogList = tradeLogService.selectList(tradeLog, pageNum, pageSize);
+        PageInfo<TradeLog> pageInfo = new PageInfo<>(orderLogList);
+
+        ResponseEntity re = ResponseEntity.ok(Utils.kv("dataList", pageInfo.getList(), "total", pageInfo.getTotal()));
+        LOGGER.info("{}", re);
+        return re;
     }
 
+    /**
+     * 批量对账单
+     */
+    @PutMapping("/bill")
+    public ResponseEntity bill(@Valid @RequestBody List<TradeBillBO> dataList) {
+        LOGGER.info("{}", dataList);
+
+        List<TradeBillBO> undoneList = tradeLogService.bill(dataList);
+
+        ResponseEntity re = ResponseEntity.ok(Utils.kv("dataList", undoneList));
+        LOGGER.info("{}", re);
+        return re;
+    }
+
+    /**
+     * 手工对账成功
+     */
+    @PutMapping("/valid/{id}")
+    public ResponseEntity valid(@PathVariable("id") String id) {
+        LOGGER.info("{}", id);
+
+        TradeLog log = new TradeLog();
+        log.setId(id);
+        log.setCompareStatus("3");
+        TradeLog data = tradeLogService.update(log);
+
+        ResponseEntity re = ResponseEntity.ok(Utils.kv("data", data));
+        LOGGER.info("{}", re);
+        return re;
+    }
+
+    /**
+     * 手工对账成功
+     */
+    @PutMapping("/invalid/{id}")
+    public ResponseEntity invalid(@PathVariable("id") String id) {
+        LOGGER.info("{}", id);
+
+        TradeLog log = new TradeLog();
+        log.setId(id);
+        log.setCompareStatus("2");
+        TradeLog data = tradeLogService.update(log);
+
+        ResponseEntity re = ResponseEntity.ok(Utils.kv("data", data));
+        LOGGER.info("{}", re);
+        return re;
+    }
 }
