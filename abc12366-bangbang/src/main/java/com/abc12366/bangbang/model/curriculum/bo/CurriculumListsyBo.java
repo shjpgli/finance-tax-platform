c5112654454d2603087bf3928aec3465560b809a
xieyanmao <maozi123>
2017-08-16 21:00:16 +0800

    修改查询最新课程接口

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/curriculum/bo/CurriculumListsyBo.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/curriculum/bo/CurriculumListsyBo.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 /**
  * 
- * 课程表
+ * 课程首页
  * 
  **/
 @SuppressWarnings("serial")
@@ -26,26 +26,15 @@ public class CurriculumListsyBo implements Serializable {
     /**课程封面**varchar(200)**/
     private String cover;
 
-	/**课程分类**varchar(64)**/
-	private String classify;
-
-	/**授课方式**varchar(64)**/
-	private String teachingMethod;
-
 	/**发布时间**datetime**/
 	private Date issueTime;
 
 	/**课程收费**tinyint(1)**/
 	private Integer isFree;
 
-	/**课程状态**tinyint(1)**/
-	private Integer status;
+    /**课程简介**longtext**/
+    private String curriculumidIntro;
 
-	/**创建人名称**varchar(30)**/
-	private String createrName;
-
-	/**修改时间**datetime**/
-	private Date updateTime;
 
     public String getCurriculumId() {
         return curriculumId;
@@ -87,22 +76,6 @@ public class CurriculumListsyBo implements Serializable {
         this.cover = cover;
     }
 
-    public String getClassify() {
-        return classify;
-    }
-
-    public void setClassify(String classify) {
-        this.classify = classify;
-    }
-
-    public String getTeachingMethod() {
-        return teachingMethod;
-    }
-
-    public void setTeachingMethod(String teachingMethod) {
-        this.teachingMethod = teachingMethod;
-    }
-
     public Date getIssueTime() {
         return issueTime;
     }
@@ -119,27 +92,11 @@ public class CurriculumListsyBo implements Serializable {
         this.isFree = isFree;
     }
 
-    public Integer getStatus() {
-        return status;
+    public String getCurriculumidIntro() {
+        return curriculumidIntro;
     }
 
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getCreaterName() {
-        return createrName;
-    }
-
-    public void setCreaterName(String createrName) {
-        this.createrName = createrName;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+    public void setCurriculumidIntro(String curriculumidIntro) {
+        this.curriculumidIntro = curriculumidIntro;
     }
 }
