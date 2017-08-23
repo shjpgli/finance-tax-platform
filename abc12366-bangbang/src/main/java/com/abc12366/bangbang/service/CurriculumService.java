0487e61f237b5c1b11fca76b738084006aa077aa
xieyanmao <maozi123>
2017-08-23 16:26:35 +0800

    新增评价统计信息接口(前端用)

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/CurriculumService.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/CurriculumService.java
@@ -26,6 +26,8 @@ public interface CurriculumService {
 
     CurriculumsyBo selectCurriculumsy(String curriculumId);
 
+    CurriculumEvaluateTjBo selectEvaluateTj(String curriculumId);
+
     CurriculumBo update(CurriculumBo curriculumBo);
 
     String updateStatus(String curriculumId, String status);
