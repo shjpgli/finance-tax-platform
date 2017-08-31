580bfd1fc4c7353c0ae979d740ef5a48c3cb8653
qiqiwill <liuqi281078208@163.com>
2017-08-31 10:19:26 +0800

    code up

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/config/WebMvcConfig.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/config/WebMvcConfig.java
@@ -87,7 +87,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
                         //获取课程详情信息
                 .excludePathPatterns("/curriculum/selectCurriculum/**")
                 .excludePathPatterns("/hotspot/**")
-                .excludePathPatterns("/knowledgeBase/list","/knowledgeBase/hotList","/knowledgeBase/interestedList/**","/knowledgeBase/relatedList/**",
+                .excludePathPatterns("/knowledgeBase/uc/list","/knowledgeBase/hotList","/knowledgeBase/interestedList/**","/knowledgeBase/relatedList/**",
                         "/knowledgeBase/vote/add","/knowledgeBase/view/**","/knowledgeBase/pv/**","/knowledgeCategory/listAll",
                         "/KnowledgeTag/listHot/**","/KnowledgeTag/listHot","/knowledgeBase/hotUnClassifyList");
 
