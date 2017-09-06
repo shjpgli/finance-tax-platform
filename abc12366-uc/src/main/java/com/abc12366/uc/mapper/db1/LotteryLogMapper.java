249c5e774a3258e54e8d269c6404f036daaae265
Administrator <554600654@qq.com>
2017-09-06 11:18:39 +0800

    积分抽奖

---
+++ abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/LotteryLogMapper.java
@@ -0,0 +1,15 @@
+package com.abc12366.uc.mapper.db1;
+
+/**
+ * Admin: lingsuzhi <554600654@qq.com.com>
+ * Date: 2017-09-06
+ */
+
+
+import com.abc12366.uc.model.LotteryLog;
+
+public interface LotteryLogMapper {
+    Integer insert(LotteryLog lotteryLog);
+    Integer update(LotteryLog lotteryLog);
+    Integer delete(String id);
+}
---
+++ abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/UpointsLotteryMapper.java
@@ -0,0 +1,15 @@
+package com.abc12366.uc.mapper.db1;
+
+/**
+ * Admin: lingsuzhi <554600654@qq.com.com>
+ * Date: 2017-09-05
+ */
+
+
+import com.abc12366.uc.model.UpointsLottery;
+
+public interface UpointsLotteryMapper {
+    Integer insert(UpointsLottery upointsLottery);
+    Integer update(UpointsLottery upointsLottery);
+    Integer delete(String id);
+}
