340f6c0337570e8ccfd82525eb4a2352058cde07
liuguiyao <435720953@qq.com>
2017-09-11 20:36:55 +0800

    配合前台修改uc用户登录加密解密方式

--- abc12366-uc/src/main/java/com/abc12366/uc/service/impl/RSAServiceImpl.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/service/impl/RSAServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import sun.misc.BASE64Decoder;
 
 import java.io.IOException;
+import java.security.interfaces.RSAPrivateKey;
 
 /**
  * User: liuguiyao<435720953@qq.com>
@@ -18,12 +19,13 @@ public class RSAServiceImpl implements RSAService {
     @Override
     public String decode(String str) {
         try {
+            RSAPrivateKey privateKey = RSA.getDefaultPrivateKey();
             byte[] bytes = new BASE64Decoder().decodeBuffer(str);
             String base64 = new String(bytes);
             String[] test = base64.split(" ");
             String json = "";
             for (String s : test) {
-                json += RSA.decryptStringByJs(s);
+                json += RSA.decryptString(privateKey, s);
             }
             return json;
         } catch (IOException e) {
