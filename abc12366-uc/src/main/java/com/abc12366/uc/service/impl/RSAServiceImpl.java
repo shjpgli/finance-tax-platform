8de269527323e23525772103232a38a75b25259d
liuguiyao <435720953@qq.com>
2017-09-11 20:22:48 +0800

    配合前台修改uc用户登录加密解密方式

--- abc12366-uc/src/main/java/com/abc12366/uc/service/impl/RSAServiceImpl.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/service/impl/RSAServiceImpl.java
@@ -3,6 +3,9 @@ package com.abc12366.uc.service.impl;
 import com.abc12366.uc.service.RSAService;
 import com.abc12366.uc.wsbssoa.utils.RSA;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Decoder;
+
+import java.io.IOException;
 
 /**
  * User: liuguiyao<435720953@qq.com>
@@ -19,6 +22,12 @@ public class RSAServiceImpl implements RSAService {
         for (String s : test) {
             json += RSA.decryptStringByJs(s);
         }
-        return json;
+        try {
+            byte[] bytes = new BASE64Decoder().decodeBuffer(str);
+            return new String(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
