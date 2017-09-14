f5bf8d1ce02ab9c99d2a2c8c7e371f36262a103a
qiqiwill <liuqi281078208@163.com>
2017-09-14 18:53:11 +0800

    专家管理

---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/question/QuestionExpert.java
@@ -0,0 +1,92 @@
+package com.abc12366.bangbang.model.question;
+import java.io.Serializable;
+
+
+/**
+ * 
+ * 
+ * 
+ **/
+@SuppressWarnings("serial")
+public class QuestionExpert implements Serializable {
+
+	/**PK**/
+	private String id;
+
+	/**uc关联用户id**/
+	private String useId;
+
+	/**专家头像**/
+	private String image;
+
+	/**擅长领域**/
+	private String expertise;
+
+	/**权限（对应字典表的bb_question_expert_permission编码）**/
+	private String permission;
+
+	/**创建时间**/
+	private java.util.Date createTime;
+
+	/**修改时间**/
+	private java.util.Date updateTime;
+
+
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return this.id;
+	}
+
+	public void setUseId(String useId){
+		this.useId = useId;
+	}
+
+	public String getUseId(){
+		return this.useId;
+	}
+
+	public void setImage(String image){
+		this.image = image;
+	}
+
+	public String getImage(){
+		return this.image;
+	}
+
+	public void setExpertise(String expertise){
+		this.expertise = expertise;
+	}
+
+	public String getExpertise(){
+		return this.expertise;
+	}
+
+	public void setPermission(String permission){
+		this.permission = permission;
+	}
+
+	public String getPermission(){
+		return this.permission;
+	}
+
+	public void setCreateTime(java.util.Date createTime){
+		this.createTime = createTime;
+	}
+
+	public java.util.Date getCreateTime(){
+		return this.createTime;
+	}
+
+	public void setUpdateTime(java.util.Date updateTime){
+		this.updateTime = updateTime;
+	}
+
+	public java.util.Date getUpdateTime(){
+		return this.updateTime;
+	}
+
+}
