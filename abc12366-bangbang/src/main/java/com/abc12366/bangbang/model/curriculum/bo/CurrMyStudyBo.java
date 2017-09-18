133c5c1a45924cdb2f3f2704c9a9d4197e4d8585
xieyanmao <maozi123>
2017-09-18 18:05:06 +0800

    修改课程相关BUG

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/curriculum/bo/CurrMyStudyBo.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/curriculum/bo/CurrMyStudyBo.java
@@ -47,6 +47,9 @@ public class CurrMyStudyBo implements Serializable {
     /**课件ID**/
     private String coursewareId;
 
+    /**学习次数**/
+    private int studyNum;
+
     public String getCurriculumId() {
         return curriculumId;
     }
@@ -141,5 +144,13 @@ public class CurrMyStudyBo implements Serializable {
 
     public void setCoursewareId(String coursewareId) {
         this.coursewareId = coursewareId;
+    }
+
+    public int getStudyNum() {
+        return studyNum;
+    }
+
+    public void setStudyNum(int studyNum) {
+        this.studyNum = studyNum;
     }
 }
