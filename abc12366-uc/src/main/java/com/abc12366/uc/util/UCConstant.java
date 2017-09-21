c0d1ea0925cff34705f46673e384d516662a89d8
liuguiyao <435720953@qq.com>
2017-09-21 16:29:08 +0800

    1.配合前台修改获取任务列表接口

--- abc12366-uc/src/main/java/com/abc12366/uc/util/UCConstant.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/util/UCConstant.java
@@ -97,4 +97,7 @@ public class UCConstant {
     //积分计算规则ID
     //补签到
     public final static String POINT_RULE_RECHECK_ID = "003a6366-0bba-4a0b-8486-9b3c2805adeb";
+    //积分兑换（该条规则不用于积分计算，仅用于展示）
+    public final static String POINT_RULE_EXCHANGE_ID = "9e945332-2001-431a-bbc7-e8c38b66572f";
+
 }
