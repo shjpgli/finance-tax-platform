ea79f74573f7202dc4e00bb45dfefcbae513c5b6
qiqiwill <liuqi281078208@163.com>
2017-09-22 18:31:12 +0800

    帮邦掌门人管理

---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/impl/QuestionHeadmanServiceImpl.java
@@ -0,0 +1,72 @@
+package com.abc12366.bangbang.service.impl;
+
+import com.abc12366.bangbang.mapper.db1.QuestionHeadmanClassifyRelMapper;
+import com.abc12366.bangbang.mapper.db1.QuestionHeadmanMapper;
+import com.abc12366.bangbang.model.question.QuestionHeadman;
+import com.abc12366.bangbang.model.question.QuestionHeadmanClassifyRel;
+import com.abc12366.bangbang.model.question.bo.QuestionHeadmanBo;
+import com.abc12366.bangbang.service.QuestionHeadmanService;
+import com.abc12366.gateway.util.Utils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author liuQi
+ * @Date 2017/9/22 18:05
+ */
+@Service
+public class QuestionHeadmanServiceImpl implements QuestionHeadmanService {
+
+    @Autowired
+    private QuestionHeadmanMapper questionHeadmanMapper;
+
+    @Autowired
+    private QuestionHeadmanClassifyRelMapper questionHeadmanClassifyRelMapper;
+
+    @Transactional("db1TxManager")
+    @Override
+    public void add(QuestionHeadmanBo headmanBo) {
+        QuestionHeadman headman = new QuestionHeadman();
+        BeanUtils.copyProperties(headmanBo, headman);
+        headman.setId(Utils.uuid());
+        headman.setUserId(Utils.getUserId());
+        questionHeadmanMapper.insert(headman);
+        List<QuestionHeadmanClassifyRel> relList = new ArrayList<>();
+        for (String classifyId:headmanBo.getClassifyIds()){
+            QuestionHeadmanClassifyRel rel = new QuestionHeadmanClassifyRel();
+            rel.setId(Utils.uuid());
+            rel.setHeadmanId(headman.getId());
+            rel.setClassifyId(classifyId);
+        }
+        questionHeadmanClassifyRelMapper.insertBatch(relList);
+    }
+
+    @Transactional("db1TxManager")
+    @Override
+    public void modify(QuestionHeadmanBo headmanBo) {
+        QuestionHeadman headman = new QuestionHeadman();
+        BeanUtils.copyProperties(headmanBo, headman);
+        questionHeadmanMapper.updateByPrimaryKeySelective(headman);
+        questionHeadmanClassifyRelMapper.deleteByHeadmanId(headman.getId());
+        List<QuestionHeadmanClassifyRel> relList = new ArrayList<>();
+        for (String classifyId:headmanBo.getClassifyIds()){
+            QuestionHeadmanClassifyRel rel = new QuestionHeadmanClassifyRel();
+            rel.setId(Utils.uuid());
+            rel.setHeadmanId(headman.getId());
+            rel.setClassifyId(classifyId);
+        }
+        questionHeadmanClassifyRelMapper.insertBatch(relList);
+    }
+
+    @Transactional("db1TxManager")
+    @Override
+    public void delete(String id) {
+        questionHeadmanMapper.deleteByPrimaryKey(id);
+        questionHeadmanClassifyRelMapper.deleteByHeadmanId(id);
+    }
+}
