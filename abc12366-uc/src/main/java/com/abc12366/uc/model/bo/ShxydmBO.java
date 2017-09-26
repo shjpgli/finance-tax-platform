75539e8cf771d22e69a96ff36c7da6154b9b455a
liuguiyao <435720953@qq.com>
2017-09-26 11:58:22 +0800

    纳税人绑定企业数量根据会员等级计算埋点

---
+++ abc12366-uc/src/main/java/com/abc12366/uc/model/bo/ShxydmBO.java
@@ -0,0 +1,18 @@
+package com.abc12366.uc.model.bo;
+
+/**
+ * User: liuguiyao<435720953@qq.com>
+ * Date: 2017-09-26
+ * Time: 11:40
+ */
+public class ShxydmBO {
+    private String shxydm;
+
+    public String getShxydm() {
+        return shxydm;
+    }
+
+    public void setShxydm(String shxydm) {
+        this.shxydm = shxydm;
+    }
+}
