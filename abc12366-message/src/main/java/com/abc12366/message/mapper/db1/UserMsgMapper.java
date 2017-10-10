0ffa97f41d012a01327b2a5afc6fb805b2582365
ljun51 <ljun51@outlook.com>
2017-10-10 14:22:04 +0800

    新增用户、业务消息群发接口

--- abc12366-message/src/main/java/com/abc12366/message/mapper/db1/UserMsgMapper.java
+++ abc12366-message/src/main/java/com/abc12366/message/mapper/db1/UserMsgMapper.java
@@ -2,6 +2,8 @@ package com.abc12366.message.mapper.db1;
 
 import com.abc12366.message.model.UserMessage;
 
+import java.util.List;
+
 /**
  * @author lijun <ljun51@outlook.com>
  * @create 2017-07-27 3:12 PM
@@ -13,4 +15,6 @@ public interface UserMsgMapper {
     void delete(String id);
 
     void update(UserMessage data);
+
+    void batchInsert(List<UserMessage> dataList);
 }
