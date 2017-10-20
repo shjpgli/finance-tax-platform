b7c876d47ed0839d69198cc2223d84b3acbd587e
liuguiyao <435720953@qq.com>
2017-10-20 16:18:15 +0800

    1.用户签到排行列表接口增加返回用户头像图片地址 2.纳税人绑定列表查询接口sql性能优化

--- abc12366-uc/src/main/java/com/abc12366/uc/model/CheckRank.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/model/CheckRank.java
@@ -3,16 +3,24 @@ package com.abc12366.uc.model;
 import java.util.Date;
 
 /**
+ * 用户签到排行返回参数实体类
  * User: liuguiyao<435720953@qq.com>
  * Date: 2017-08-21
  * Time: 17:00
  */
 public class CheckRank {
     private String id;
+    //用户ID
     private String userId;
+    //用户头像图片路径
+    private String userPicturePath;
+    //用户昵称
     private String nickname;
+    //最后更新时间
     private Date lastUpdate;
+    //年内累计签到统计
     private Integer count;
+    //年份
     private String year;
 
     public CheckRank() {
@@ -64,5 +72,26 @@ public class CheckRank {
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    public String getUserPicturePath() {
+        return userPicturePath;
+    }
+
+    public void setUserPicturePath(String userPicturePath) {
+        this.userPicturePath = userPicturePath;
+    }
+
+    @Override
+    public String toString() {
+        return "CheckRank{" +
+                "id='" + id + '\'' +
+                ", userId='" + userId + '\'' +
+                ", userPicturePath='" + userPicturePath + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", lastUpdate=" + lastUpdate +
+                ", count=" + count +
+                ", year='" + year + '\'' +
+                '}';
     }
 }
