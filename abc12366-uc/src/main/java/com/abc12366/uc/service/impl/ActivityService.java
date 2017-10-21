8b7b0a7f4d9488dac3489958fd05e4ccbfd855fd
ljun51 <ljun51@outlook.com>
2017-10-21 14:37:05 +0800

    修复微信红包问题

--- abc12366-uc/src/main/java/com/abc12366/uc/service/impl/ActivityService.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/service/impl/ActivityService.java
@@ -292,8 +292,8 @@ public class ActivityService implements IActivityService {
     @Override
     public WxRedEnvelop resend(String id) {
         WxRedEnvelop redEnvelop = activityRoMapper.selectRedEnvelopOne(id);
-        // 发送失败的记录
-        if (redEnvelop != null && "2".equals(redEnvelop.getSendStatus())) {
+        // 重新发送状态为【0-未发送、2-发送失败】的记录
+        if (redEnvelop != null && ("0".equals(redEnvelop.getSendStatus()) || "2".equals(redEnvelop.getSendStatus()))) {
             WxActivity activity = selectOne(redEnvelop.getActivityId());
             SendRedPack srp = new SendRedPack.Builder()
                     .nonce_str(redEnvelop.getId())
