54ca0dfaac91795c0599ed1aabb69e4b3b7b03e4
xieyanmao <maozi123>
2017-10-23 21:06:26 +0800

    更新课程浏览量接口无需登录

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/config/WebMvcConfig.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/config/WebMvcConfig.java
@@ -88,6 +88,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
                 .excludePathPatterns("/curriculum/selectListWatch")
                         //获取推荐课程
                 .excludePathPatterns("/curriculum/selectRecommend")
+                        //课程浏览量增加
+                .excludePathPatterns("/curriculum/updateBrowsesDay/**")
                         //获取活动
                 .excludePathPatterns("/event/**")
                         //获取课程详情信息
