ca2f68f74e6505f58a40103887f165eefe09af5e
ljun51 <ljun51@outlook.com>
2017-10-26 18:31:02 +0800

    删除中文乱码注释

--- abc12366-uc/src/main/java/com/abc12366/uc/jrxt/model/util/CrypTdpsUtil.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/jrxt/model/util/CrypTdpsUtil.java
@@ -9,12 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.security.Security;
 
-/**
- * 加密解密工具类
- *
- * @author ziben
- * @version 1.0 11-3-29 上午11:36
- */
 @Component
 public class CrypTdpsUtil implements InitializingBean {
     @Autowired
@@ -35,49 +29,18 @@ public class CrypTdpsUtil implements InitializingBean {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-    /**
-     * 此函数使用用户选择的加密算法对文件进行加密，目前为“DES”算法
-     *
-     * @param key         用户提供的加密算法的密钥,即申报密码,长度固定为8
-     * @param inFileName  需要进行加密的文件名（全路径）
-     * @param outFileName 指定的加密后输出文件（全路径）
-     * @return 如果成功返回0；如果文件不能打开返回1；如果提供的密钥的长度不合法返回2；如果提供的密钥内容不合法返回3；如果选择的算法不合法返回4
-     */
     public int encryptFile(String key, String inFileName, String outFileName) {
-        //todo:等待总局标准实现
         return -1;
     }
 
-    /**
-     * 此函数对指定文件使用指定的加密算法进行解密，目前为“DES”算法
-     *
-     * @param key         用户提供的加密算法的密钥,即申报密码
-     * @param inFileName  需要进行加密的文件名（全路径）
-     * @param outFileName 指定的加密后输出文件（全路径）
-     * @return 如果成功返回0；如果文件不能打开返回1；如果提供的密钥的长度不合法返回2；如果提供的密钥内容不合法返回3；如果选择的算法不合法返回4
-     */
     public int decryptFile(String key, String inFileName, String outFileName) {
-        //todo:等待总局标准实现
-
         return -1;
     }
 
-    /**
-     * 利用RSA算法对字符串进行加密
-     *
-     * @param message 需要加密的信息
-     * @return 加密后的消息，base64格式
-     */
     public String b64AsymmetricEncrypt(String message) {
         return this.b64AsymmetricEncrypter.encrypt(message);
     }
 
-    /**
-     * 利用RSA算法解密字符串
-     *
-     * @param message 经过加密了的信息，base64格式
-     * @return 解密后的字符串
-     */
     public String b64AsymmetricDecrypt(String message) {
         return this.b64AsymmetricDecrypter.encrypt(message);
     }
@@ -90,15 +53,6 @@ public class CrypTdpsUtil implements InitializingBean {
         return this.symmetricDecrypter.encrypt(key, iv, message);
     }
 
-
-    /**
-     * 对传入的字节流进行加密
-     *
-     * @param byteContent
-     * @param key
-     * @return
-     * @throws Exception
-     */
     public byte[] encryptContent(byte[] byteContent, byte[] key) throws Exception {
         return this.symmetricEncrypt(key, byteContent);
     }
