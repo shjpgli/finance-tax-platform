881e5e83bccfa22e57e9debdf0f0754cd45b474d
qiqiwill <liuqi281078208@163.com>
2017-10-28 17:46:07 +0800

    帮派积分分配接口管理

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/impl/QuestionFactionRewardSettingServiceImpl.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/impl/QuestionFactionRewardSettingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.abc12366.bangbang.service.impl;
 
+import com.abc12366.bangbang.mapper.db1.QuestionFactionMapper;
 import com.abc12366.bangbang.mapper.db1.QuestionFactionRewardSettingMapper;
 import com.abc12366.bangbang.mapper.db2.QuestionFactionRewardSettingRoMapper;
 import com.abc12366.bangbang.model.question.QuestionFactionRewardSetting;
@@ -9,6 +10,7 @@ import com.abc12366.bangbang.service.QuestionFactionRewardSettingService;
 import com.abc12366.gateway.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,6 +20,9 @@ import java.util.List;
  */
 @Service
 public class QuestionFactionRewardSettingServiceImpl implements QuestionFactionRewardSettingService {
+
+    @Autowired
+    private QuestionFactionMapper questionFactionMapper;
 
     @Autowired
     private QuestionFactionRewardSettingMapper questionFactionRewardSettingMapper;
@@ -30,10 +35,12 @@ public class QuestionFactionRewardSettingServiceImpl implements QuestionFactionR
         return questionFactionRewardSettingRoMapper.selectFactionRewardSettingList(param);
     }
 
+    @Transactional("db1TxManager")
     @Override
     public void setting(QuestionFactionRewardSetting record) {
         record.setId(Utils.uuid());
         record.setUpdateAdmin(Utils.getAdminId());
         questionFactionRewardSettingMapper.insert(record);
+        questionFactionMapper.updateAwardPoint(record.getRewardsPoints());
     }
 }
