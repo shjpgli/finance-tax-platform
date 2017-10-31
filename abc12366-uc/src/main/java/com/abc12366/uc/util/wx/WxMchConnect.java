4bdbb0d306990836654fa0b7c0effd1e2cfcd87e
ljun51 <ljun51@outlook.com>
2017-10-31 20:39:25 +0800

    修复微信红包问题

--- abc12366-uc/src/main/java/com/abc12366/uc/util/wx/WxMchConnect.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/util/wx/WxMchConnect.java
@@ -15,12 +15,10 @@ import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.ResourceUtils;
 
 import javax.net.ssl.SSLContext;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.KeyStore;
 import java.util.Map;
@@ -68,8 +66,7 @@ public class WxMchConnect<T> {
 
     public void httpsRequest() throws Exception {
         KeyStore keyStore  = KeyStore.getInstance("PKCS12");
-        File pkcs12 = new ClassPathResource("cer/apiclient_cert.p12").getFile();
-        FileInputStream instream = new FileInputStream(pkcs12);
+        InputStream instream = new ClassPathResource("cer/apiclient_cert.p12").getInputStream();
         String mch_id = SpringCtxHolder.getProperty("abc.mch_id");
         try {
             keyStore.load(instream, mch_id.toCharArray());
