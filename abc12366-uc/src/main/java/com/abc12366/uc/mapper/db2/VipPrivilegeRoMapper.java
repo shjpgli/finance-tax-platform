eede4a33f3d4664d46afebe4efcc509c9dd2ef19
ljun51 <ljun51@outlook.com>
2017-11-08 15:24:54 +0800

    新增特权代码

--- abc12366-uc/src/main/java/com/abc12366/uc/mapper/db2/VipPrivilegeRoMapper.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/mapper/db2/VipPrivilegeRoMapper.java
@@ -1,17 +1,32 @@
 package com.abc12366.uc.mapper.db2;
 
-import com.abc12366.uc.model.bo.VipPrivilegeBO;
+import com.abc12366.uc.model.VipPrivilege;
 
 import java.util.List;
 import java.util.Map;
 
 /**
- * Admin: liuguiyao<435720953@qq.com.com>
- * Date: 2017-05-22
- * Time: 9:57
+ * 特权服务
+ *
+ * @author lijun <ljun51@outlook.com>
+ * @create 2017-11-08 4:02 PM
+ * @since 1.0.0
  */
 public interface VipPrivilegeRoMapper {
-    List<VipPrivilegeBO> selectList(Map map);
 
-    VipPrivilegeBO selectOne(String id);
+    /**
+     * 查询会员特权列表
+     *
+     * @param map name,status属性
+     * @return 员特权列表
+     */
+    List<VipPrivilege> selectList(Map map);
+
+    /**
+     * 根据ID查看会员特权
+     *
+     * @param id PK
+     * @return VipPrivilege
+     */
+    VipPrivilege selectOne(String id);
 }
--- abc12366-uc/src/main/java/com/abc12366/uc/model/VipPrivilege.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/model/VipPrivilege.java
@@ -3,20 +3,133 @@ package com.abc12366.uc.model;
 import java.util.Date;
 
 /**
- * Admin: liuguiyao<435720953@qq.com.com>
- * Date: 2017-05-22
- * Time: 9:47
+ * 会员特权对象
+ *
+ * @author lijun <ljun51@outlook.com>
+ * @create 2017-11-08 4:02 PM
+ * @since 1.0.0
  */
 public class VipPrivilege {
     private String id;
-    private String name;//特权名称
-    private String icon;//图标
-    private String description;//描述
-    private Boolean status;//数据状态
 
+    /**
+     * 特权名称
+     */
+    private String name;
+
+    /**
+     * 特权代码
+     */
+    private String code;
+
+    /**
+     * 图标
+     */
+    private String icon;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 数据状态
+     */
+    private Boolean status;
+
+    /**
+     * 更新时间
+     */
     private Date lastUpdate;
+
+    /**
+     * 创建时间
+     */
     private Date createTime;
-    private Integer sort;//排序
+
+    /**
+     * 排序
+     */
+    private Integer sort;
+
+    public VipPrivilege() {
+    }
+
+    private VipPrivilege(Builder builder) {
+        setId(builder.id);
+        setName(builder.name);
+        setCode(builder.code);
+        setIcon(builder.icon);
+        setDescription(builder.description);
+        setStatus(builder.status);
+        setLastUpdate(builder.lastUpdate);
+        setCreateTime(builder.createTime);
+        setSort(builder.sort);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
     public Integer getSort() {
         return sort;
@@ -28,50 +141,81 @@ public class VipPrivilege {
 
     @Override
     public String toString() {
-        return "VipPrivilege [id=" + id + ", name=" + name + ", icon=" + icon + ", description=" + description + ", status="
-                + status + ", lastUpdate=" + lastUpdate + ", createTime=" + createTime + "]";
+        return "VipPrivilege{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", icon='" + icon + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", lastUpdate=" + lastUpdate +
+                ", createTime=" + createTime +
+                ", sort=" + sort +
+                '}';
     }
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getIcon() {
-        return icon;
-    }
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public Boolean getStatus() {
-        return status;
-    }
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-    public Date getLastUpdate() {
-        return lastUpdate;
-    }
-    public void setLastUpdate(Date lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-    public Date getCreateTime() {
-        return createTime;
-    }
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
+
+    public static final class Builder {
+        private String id;
+        private String name;
+        private String code;
+        private String icon;
+        private String description;
+        private Boolean status;
+        private Date lastUpdate;
+        private Date createTime;
+        private Integer sort;
+
+        public Builder() {
+        }
+
+        public Builder id(String val) {
+            id = val;
+            return this;
+        }
+
+        public Builder name(String val) {
+            name = val;
+            return this;
+        }
+
+        public Builder code(String val) {
+            code = val;
+            return this;
+        }
+
+        public Builder icon(String val) {
+            icon = val;
+            return this;
+        }
+
+        public Builder description(String val) {
+            description = val;
+            return this;
+        }
+
+        public Builder status(Boolean val) {
+            status = val;
+            return this;
+        }
+
+        public Builder lastUpdate(Date val) {
+            lastUpdate = val;
+            return this;
+        }
+
+        public Builder createTime(Date val) {
+            createTime = val;
+            return this;
+        }
+
+        public Builder sort(Integer val) {
+            sort = val;
+            return this;
+        }
+
+        public VipPrivilege build() {
+            return new VipPrivilege(this);
+        }
     }
 }
 
