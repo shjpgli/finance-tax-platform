c6ee9191c77410589ae715a14ac00979e4166073
xieyanmao <maozi123>
2017-11-13 14:50:11 +0800

    新增邦派任务相关接口

---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/QueFactionTaskService.java
@@ -0,0 +1,17 @@
+package com.abc12366.bangbang.service;
+
+
+
+import com.abc12366.bangbang.model.question.bo.QuestionFactionPhBo;
+import com.abc12366.bangbang.model.question.bo.QuestionFactionTaskBo;
+
+import java.util.List;
+import java.util.Map;
+
+public interface QueFactionTaskService {
+
+    List<QuestionFactionTaskBo> selectListdt();
+
+    List<QuestionFactionTaskBo> selectTaskList(Map map);
+
+}
---
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/impl/QueFactionTaskServiceImpl.java
@@ -0,0 +1,54 @@
+package com.abc12366.bangbang.service.impl;
+
+import com.abc12366.bangbang.mapper.db2.QuestionFactionHonorRoMapper;
+import com.abc12366.bangbang.mapper.db2.QuestionFactionTaskRoMapper;
+import com.abc12366.bangbang.model.question.bo.QuestionFactionPhBo;
+import com.abc12366.bangbang.model.question.bo.QuestionFactionTaskBo;
+import com.abc12366.bangbang.service.QueFactionHonorService;
+import com.abc12366.bangbang.service.QueFactionTaskService;
+import com.abc12366.gateway.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xieyanmao on 2017/9/14.
+ */
+@Service
+public class QueFactionTaskServiceImpl implements QueFactionTaskService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueFactionTaskServiceImpl.class);
+
+    @Autowired
+    private QuestionFactionTaskRoMapper taskRoMapper;
+
+    @Override
+    public List<QuestionFactionTaskBo> selectListdt() {
+        List<QuestionFactionTaskBo> taskBoList;
+        try {
+            //查询邦派动态列表
+            taskBoList = taskRoMapper.selectListdt();
+        } catch (Exception e) {
+            LOGGER.error("查询邦派动态列表信息异常：{}", e);
+            throw new ServiceException(6120);
+        }
+        return taskBoList;
+    }
+
+    @Override
+    public List<QuestionFactionTaskBo> selectTaskList(Map map) {
+        List<QuestionFactionTaskBo> taskBoList;
+        try {
+            //查询邦派任务情况
+            taskBoList = taskRoMapper.selectTaskList(map);
+        } catch (Exception e) {
+            LOGGER.error("查询邦派任务情况信息异常：{}", e);
+            throw new ServiceException(6120);
+        }
+        return taskBoList;
+    }
+
+}
