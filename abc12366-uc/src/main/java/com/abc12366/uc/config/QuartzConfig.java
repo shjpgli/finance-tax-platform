1ed555199c59290f6f5b039eeb5f33073b2c4c8e
zhushuai <xg214zs@163.com>
2017-12-01 15:50:45 +0800

    修改定时人线程数量

--- abc12366-uc/src/main/java/com/abc12366/uc/config/QuartzConfig.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/config/QuartzConfig.java
@@ -66,7 +66,7 @@ public class QuartzConfig {
         prop.put("org.quartz.jobStore.tablePrefix", "QRTZ_");
         prop.put("org.quartz.jobStore.isClustered", "true");
         prop.put("org.quartz.threadPool.class", "org.quartz.simpl.SimpleThreadPool");
-        prop.put("org.quartz.threadPool.threadCount", "5");
+        prop.put("org.quartz.threadPool.threadCount", "20");
 
         prop.put("org.quartz.dataSource.quartzDataSource.driver", driver);
         prop.put("org.quartz.dataSource.quartzDataSource.URL", url);
