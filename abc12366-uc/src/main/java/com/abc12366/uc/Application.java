3cd41f75c010c7a3ea4b7b063de14e31b1d9d4e4
ljun51 <ljun51@outlook.com>
2017-12-05 14:13:25 +0800

    修复用户列表查询注册时间查询条件

--- abc12366-uc/src/main/java/com/abc12366/uc/Application.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
