9e526649ff243608451a0e5a3d78b614f62f979f
zhushuai <xg214zs@163.com>
2017-12-07 14:32:32 +0800

    修改网关操作日志bug

--- abc12366-gateway/src/main/java/com/abc12366/gateway/service/AdminLogServiceImpl.java
+++ abc12366-gateway/src/main/java/com/abc12366/gateway/service/AdminLogServiceImpl.java
@@ -37,7 +37,7 @@ public class AdminLogServiceImpl implements AdminLogService {
                 .createTime(now)
                 .businessUri(bo.getBusinessUri())
                 .businessName(bo.getBusinessName())
-                .businessData(bo.getBusinessData())
+                .businessData((bo.getBusinessData().getBytes().length>4000?" ":bo.getBusinessData()))
                 .method(bo.getMethod())
                 .remark(bo.getRemark())
                 .build();
@@ -57,4 +57,5 @@ public class AdminLogServiceImpl implements AdminLogService {
         PageHelper.startPage(page, size, true).pageSizeZero(true).reasonable(true);
         return adminLogRoMapper.selectList(adminLog);
     }
+    
 }
