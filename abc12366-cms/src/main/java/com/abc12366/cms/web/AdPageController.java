ed0c3f0b1e7371a6de33f855d8981006f23aa1ea
zhushuai <xg214zs@163.com>
2017-12-14 11:31:59 +0800

    1. 客户端：我的首页：广告轮播图接口 /adpages 2. 客户端：我的首页：财税资讯列表 /content/selectListcszxwForqt

--- abc12366-cms/src/main/java/com/abc12366/cms/web/AdPageController.java
+++ abc12366-cms/src/main/java/com/abc12366/cms/web/AdPageController.java
@@ -1,18 +1,23 @@
 package com.abc12366.cms.web;
 
 import com.abc12366.cms.model.bo.AdPageBO;
+import com.abc12366.cms.model.bo.ContentsListBo;
 import com.abc12366.cms.service.AdPageService;
 import com.abc12366.gateway.util.Constant;
+import com.abc12366.gateway.util.RedisConstant;
 import com.abc12366.gateway.util.Utils;
+import com.alibaba.fastjson.JSONArray;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -27,6 +32,9 @@ public class AdPageController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AdPageController.class);
     @Autowired
     private AdPageService adPageService;
+    
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @GetMapping("/adpage")
     public ResponseEntity selectList(@RequestParam(value = "name", required = false) String name,
@@ -50,7 +58,7 @@ public class AdPageController {
     public ResponseEntity selectListForqt(@RequestParam(value = "name", required = false) String name,
                                           @RequestParam(value = "page", defaultValue = Constant.pageNum) int page,
                                           @RequestParam(value = "size", defaultValue = Constant.pageSize) int size) {
-        LOGGER.info("{},{},{}", name, page, size);
+        /*LOGGER.info("{},{},{}", name, page, size);
 
         AdPageBO adPage = new AdPageBO();
         adPage.setName(name);
@@ -60,9 +68,25 @@ public class AdPageController {
         ResponseEntity responseEntity = ResponseEntity.ok(Utils.kv("dataList", pageInfo.getList(),
                 "total", pageInfo.getTotal()));
 
-        LOGGER.info("{}", responseEntity);
-        return responseEntity;
+        LOGGER.info("{}", responseEntity);*/
+    	//redisTemplate.delete("CMS_AdpageListFqt");
+    	if(redisTemplate.hasKey("CMS_AdpageListFqt")){
+    		List<AdPageBO> dataList = JSONArray.parseArray(redisTemplate.opsForValue().get("CMS_AdpageListFqt"),AdPageBO.class);
+    		LOGGER.info("从Redis获取数据:"+JSONArray.toJSONString(dataList));
+    		return ResponseEntity.ok(Utils.kv("dataList", dataList, "total", dataList.size()));
+    	}else{
+    		AdPageBO adPage = new AdPageBO();
+	        adPage.setName(name);
+	        List<AdPageBO> dataList = adPageService.selectListForqt(adPage, 0, 0);
+	        redisTemplate.opsForValue().set("CMS_AdpageListFqt",JSONArray.toJSONString(dataList),RedisConstant.USER_INFO_TIME_ODFAY, TimeUnit.DAYS);
+	        PageInfo<AdPageBO> pageInfo = new PageInfo<AdPageBO>(dataList);
+	        return ResponseEntity.ok(Utils.kv("dataList", pageInfo.getList(),
+	                "total", pageInfo.getTotal()));
+    	}
     }
+   
+    
+    
 
     @PostMapping("/adpage")
     public ResponseEntity insert(@Valid @RequestBody AdPageBO adPage) {
