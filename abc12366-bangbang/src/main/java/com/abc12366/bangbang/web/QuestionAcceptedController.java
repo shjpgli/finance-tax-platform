949f5a0fc22907157267c5c95b1c049f2cb68c62
zhushuai <xg214zs@163.com>
2017-12-19 15:04:26 +0800

    缓存查询 Bangb_Accepted /statiss

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/web/QuestionAcceptedController.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/web/QuestionAcceptedController.java
@@ -5,17 +5,21 @@ import com.abc12366.bangbang.model.question.bo.QuestionAcceptedBO;
 import com.abc12366.bangbang.service.QuestionAcceptedService;
 import com.abc12366.gateway.util.Constant;
 import com.abc12366.gateway.util.DateUtils;
+import com.abc12366.gateway.util.RedisConstant;
 import com.abc12366.gateway.util.Utils;
+import com.alibaba.fastjson.JSONArray;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author lizhongwei
@@ -32,6 +36,9 @@ public class QuestionAcceptedController {
     @Autowired
     private QuestionAcceptedService questionAcceptedService;
 
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     /**
      * 问题受理 分页查询-后台
@@ -134,6 +141,41 @@ public class QuestionAcceptedController {
                 ResponseEntity.ok(Utils.kv()) :
                 ResponseEntity.ok(Utils.kv("dataList", pageInfo.getList(), "total", pageInfo.getTotal()));
     }
+    
+    /**
+     * 问题受理 统计查询(缓存)
+     * @param userId    用户ID
+     * @param date  时间
+     * @return
+     */
+    @GetMapping(path = "/statiss")
+    public ResponseEntity selectStatisLists(
+                                     @RequestParam(value = "userId", required = false) String userId,
+                                     @RequestParam(value = "date", required = false) String date) {
+    	if(redisTemplate.hasKey("Bangb_Accepted")){
+    		List<QuestionAcceptedBO> list = JSONArray.parseArray(redisTemplate.opsForValue().get("Bangb_Accepted"),QuestionAcceptedBO.class);
+    		LOGGER.info("从Redis获取数据:"+JSONArray.toJSONString(list));
+    		return (list == null) ?
+                    ResponseEntity.ok(Utils.kv()) :
+                    ResponseEntity.ok(Utils.kv("dataList", list, "total", list.size()));
+    	}else{
+    		PageHelper.startPage(0, 0, true).pageSizeZero(true).reasonable(true);
+            QuestionAcceptedBO param = new QuestionAcceptedBO();
+            if(date != null && !"".equals(date)) {
+                param.setDate(date);
+            }
+            param.setUserId(userId);
+            List<QuestionAcceptedBO> list = questionAcceptedService.selectStatisList(param);
+            redisTemplate.opsForValue().set("Bangb_Accepted", JSONArray.toJSONString(list),RedisConstant.USER_INFO_TIME_ODFAY, TimeUnit.DAYS);
+
+            PageInfo<QuestionAcceptedBO> pageInfo = new PageInfo<>(list);
+
+            return (list == null) ?
+                    ResponseEntity.ok(Utils.kv()) :
+                    ResponseEntity.ok(Utils.kv("dataList", pageInfo.getList(), "total", pageInfo.getTotal()));
+    	}
+        
+    }
 
     /***
      * 添加问题受理接口
@@ -141,6 +183,7 @@ public class QuestionAcceptedController {
     @PostMapping(path = "/add")
     public ResponseEntity add(@RequestBody QuestionAccepted QuestionAccepted) {
         questionAcceptedService.add(QuestionAccepted);
+        redisTemplate.delete("Bangb_Accepted");
         return ResponseEntity.ok(Utils.kv("data", QuestionAccepted));
     }
 
@@ -150,6 +193,7 @@ public class QuestionAcceptedController {
     @DeleteMapping(path = "/delete/{id}")
     public ResponseEntity delete(@PathVariable String id) {
         questionAcceptedService.delete(id);
+        redisTemplate.delete("Bangb_Accepted");
         return ResponseEntity.ok(Utils.kv());
     }
 
