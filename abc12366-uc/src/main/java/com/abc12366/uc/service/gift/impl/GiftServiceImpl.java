0c01f8d5f8cc93bcc151e6a873e636d98b174814
lizhongwei <Alone.weizai99>
2017-12-29 19:07:36 +0800

    会员礼包QC  bug修复

--- abc12366-uc/src/main/java/com/abc12366/uc/service/gift/impl/GiftServiceImpl.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/service/gift/impl/GiftServiceImpl.java
@@ -211,8 +211,9 @@ public class GiftServiceImpl implements GiftService {
                 LOGGER.info("新增礼包申请表与礼包关联表异常：{}", inst);
                 throw new ServiceException(4101);
             }
+            User user = userRoMapper.selectOne(userId);
             //加入礼包申请日志
-            insertUgiftLog(applyId,"","","礼物申请新增","1");
+            insertUgiftLog(applyId,user.getId(),user.getNickname(),"礼物申请新增","1");
         }
     }
 
