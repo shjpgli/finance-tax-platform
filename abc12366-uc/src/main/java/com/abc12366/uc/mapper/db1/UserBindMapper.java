d03ecef4fb5e89de75588ca06312950de0fb56f4
zhushuai <xg214zs@163.com>
2018-01-04 16:10:25 +0800

    修改绑定关系查询 都从主库查询

--- abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/UserBindMapper.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/UserBindMapper.java
@@ -3,7 +3,16 @@ package com.abc12366.uc.mapper.db1;
 import com.abc12366.uc.model.UserDzsb;
 import com.abc12366.uc.model.UserHnds;
 import com.abc12366.uc.model.UserHngs;
+import com.abc12366.uc.model.abc4000.NSRXX;
+import com.abc12366.uc.model.abc4000.NSRXXBO;
+import com.abc12366.uc.model.bo.ShxydmBO;
+import com.abc12366.uc.model.bo.UserDzsbListBO;
+import com.abc12366.uc.model.bo.UserHndsBO;
+import com.abc12366.uc.model.bo.UserHngsInsertBO;
+import com.abc12366.uc.model.bo.UserHngsListBO;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,4 +38,47 @@ public interface UserBindMapper {
     int updateBatch(Map<String,Object> map);
 
     int updateHngs(UserHngs userHngs);
+    
+    
+    UserDzsb userDzsbSelectById(String id);
+
+    UserHngs userHngsSelectById(String id);
+
+    UserHnds userHndsSelectById(String id);
+
+    List<UserDzsbListBO> getUserDzsbBind(Map<String, String> map);
+
+    List<UserHngsListBO> getUserhngsBind(Map<String, String> map);
+
+    List<UserHndsBO> getUserhndsBind(Map<String, String> map);
+
+    List<NSRXX> selectListByUserId(String userId);
+
+    List<UserHngs> userHngsListExist(UserHngsInsertBO userHngsInsertBO);
+
+    /**
+     * 根据'用户ID，纳税人识别号或社会信用代码'查询是否存在绑定信息
+     *
+     * @param queryParam 纳税人识别号、社会信用代码必传
+     * @return 纳税人绑定信息
+     */
+    List<NSRXXBO> selectListByUserIdAndNsrsbhOrShxydm(UserDzsb queryParam);
+
+    /**
+     * 根据'用户ID，纳税人识别号'查询是否存在绑定信息
+     *
+     * @param queryParam 纳税人识别号必传
+     * @return 纳税人绑定信息
+     */
+    List<NSRXXBO> selectListByUserIdAndNsrsbh(UserDzsb queryParam);
+
+    List<UserDzsb> dzsbCount(String userId);
+
+    List<UserHngs> hngsCount(String userId);
+
+    List<UserHnds> hndsCount(String userId);
+
+    List<ShxydmBO> bindCount(String userId);
+
+    List<String> selectListByDate(Date date);
 }
