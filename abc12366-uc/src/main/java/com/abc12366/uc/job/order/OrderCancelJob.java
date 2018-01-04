ae2293da57fa1cffddd59f88b2d56da4def139aa
ljun51 <ljun51@outlook.com>
2018-01-04 17:52:05 +0800

    格式化代码

--- abc12366-uc/src/main/java/com/abc12366/uc/job/order/OrderCancelJob.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/job/order/OrderCancelJob.java
@@ -3,7 +3,6 @@ package com.abc12366.uc.job.order;
 import com.abc12366.uc.service.order.OrderService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,7 @@ public class OrderCancelJob implements Job {
     private OrderService orderService;
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
         LOGGER.info("OrderCancelJob: {}", context.getJobDetail().getKey().getName());
         orderService.automaticCancel();
     }
