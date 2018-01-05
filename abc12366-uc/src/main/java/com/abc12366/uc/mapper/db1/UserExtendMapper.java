c580a3c86b278fd47f30882093a52484a5cef736
zhushuai <xg214zs@163.com>
2018-01-05 18:10:15 +0800

    用户扩展信息主库查询

--- abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/UserExtendMapper.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/mapper/db1/UserExtendMapper.java
@@ -1,6 +1,10 @@
 package com.abc12366.uc.mapper.db1;
 
+import java.util.List;
+import java.util.Map;
+
 import com.abc12366.uc.model.UserExtend;
+import com.abc12366.uc.model.bo.UserExtendListBO;
 
 /**
  * Created by lgy on 2017-05-05.
@@ -11,4 +15,19 @@ public interface UserExtendMapper {
     int delete(String userId);
 
     int update(UserExtend userExtend);
+    
+    //以下合并查询主库
+    UserExtend selectOne(String userId);
+
+    List<UserExtendListBO> selectList(Map map);
+
+    UserExtend isRealName(String userId);
+
+    UserExtend selectOneForAdmin(String userId);
+
+    /**
+     * 查询认证状态为【待认证】数量
+     * @return Integer 数量
+     */
+    Integer selectTodoListCount();
 }
