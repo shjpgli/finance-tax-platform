30b74dd5b6a9cd389936e2fc4df312dec2a17968
zhushuai <xg214zs@163.com>
2018-03-06 09:38:10 +0800

    消息发送添加是否忽略会员等级限制

--- abc12366-uc/src/main/java/com/abc12366/uc/model/MessageSendBo.java
+++ abc12366-uc/src/main/java/com/abc12366/uc/model/MessageSendBo.java
@@ -6,34 +6,55 @@ import java.util.Map;
 
 /**
  * 前段消息发送接口类
+ * 
  * @author zhushuai 2017-11-13
  *
  */
-public class MessageSendBo implements Serializable{
+public class MessageSendBo implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String userId;//用户ID
-		
-	private List<String> userIds;//批量用户ID
-	
-	private String type;//消息类型
-	
-	private String busiType;//业务类型
-	
-	private String businessId;//来源ID
-	
-	private String webMsg;//站内消息
-	
-	private String phoneMsg;//短信消息
-	
-	private Map<String, String> dataList;//模板消息内容
-	
-	private String templateid;//模板ID
+	private String userId;// 用户ID
 
-	private String skipUrl;//超链接地址
+	private List<String> userIds;// 批量用户ID
+
+	private String type;// 消息类型
+
+	private String busiType;// 业务类型
+
+	private String businessId;// 来源ID
+
+	private String webMsg;// 站内消息
+
+	private String phoneMsg;// 短信消息
+
+	private Boolean wxNoChargeVip;// 微信消息是否无视会员等级
+
+	private Boolean moblieNoChargeVip;// 短信消息是否无视会员等级
+
+	private Map<String, String> dataList;// 模板消息内容
+
+	private String templateid;// 模板ID
+
+	private String skipUrl;// 超链接地址
+
+	public Boolean getWxNoChargeVip() {
+		return wxNoChargeVip == null ? false : wxNoChargeVip;
+	}
+
+	public void setWxNoChargeVip(Boolean wxNoChargeVip) {
+		this.wxNoChargeVip = wxNoChargeVip;
+	}
+
+	public Boolean getMoblieNoChargeVip() {
+		return moblieNoChargeVip == null ? false : moblieNoChargeVip;
+	}
+
+	public void setMoblieNoChargeVip(Boolean moblieNoChargeVip) {
+		this.moblieNoChargeVip = moblieNoChargeVip;
+	}
 
 	public String getUserId() {
 		return userId;
