2ec24b76edee2178ae30d44affe47ae0812b5e41
ljun51 <ljun51@outlook.com>
2018-03-15 11:11:26 +0800

    修改关注讲师业务

--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/bo/FollowLecturerBO.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/model/bo/FollowLecturerBO.java
@@ -8,85 +8,95 @@ import java.io.Serializable;
 
 
 /**
- * 
  * 用户关注讲师表
- * 
  **/
 @SuppressWarnings("serial")
 public class FollowLecturerBO implements Serializable {
 
-	/**主键**/
-	private String id;
+    /**
+     * 主键
+     **/
+    private String id;
 
-	/**用户ID**/
+    /**
+     * 用户ID
+     **/
     @NotEmpty
     @Size(min = 6, max = 64)
-	private String userId;
+    private String userId;
 
-	/**被关注讲师ID**/
+    /**
+     * 被关注讲师ID
+     **/
     @NotEmpty
     @Size(min = 6, max = 64)
-	private String lecturerId;
+    private String lecturerId;
 
-	/**关注状态：0-取消关注，1-已关注**/
-	private Integer status;
+    /**
+     * 关注状态：0-取消关注，1-已关注
+     **/
+    private Boolean status;
 
-	/**创建时间**/
-	private java.util.Date createTime;
+    /**
+     * 创建时间
+     **/
+    private java.util.Date createTime;
 
-	/**更新时间**/
-	private java.util.Date lastUpdate;
+    /**
+     * 更新时间
+     **/
+    private java.util.Date lastUpdate;
 
     private CurriculumLecturerBo curriculumLecturer;
 
 
-	public void setId(String id){
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getId(){
-		return this.id;
-	}
+    public String getId() {
+        return this.id;
+    }
 
-	public void setUserId(String userId){
-		this.userId = userId;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public String getUserId(){
-		return this.userId;
-	}
+    public String getUserId() {
+        return this.userId;
+    }
 
-	public void setLecturerId(String lecturerId){
-		this.lecturerId = lecturerId;
-	}
+    public void setLecturerId(String lecturerId) {
+        this.lecturerId = lecturerId;
+    }
 
-	public String getLecturerId(){
-		return this.lecturerId;
-	}
+    public String getLecturerId() {
+        return this.lecturerId;
+    }
 
-	public void setStatus(Integer status){
-		this.status = status;
-	}
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 
-	public Integer getStatus(){
-		return this.status;
-	}
+    public Boolean getStatus() {
+        return this.status;
+    }
 
-	public void setCreateTime(java.util.Date createTime){
-		this.createTime = createTime;
-	}
+    public void setCreateTime(java.util.Date createTime) {
+        this.createTime = createTime;
+    }
 
-	public java.util.Date getCreateTime(){
-		return this.createTime;
-	}
+    public java.util.Date getCreateTime() {
+        return this.createTime;
+    }
 
-	public void setLastUpdate(java.util.Date lastUpdate){
-		this.lastUpdate = lastUpdate;
-	}
+    public void setLastUpdate(java.util.Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 
-	public java.util.Date getLastUpdate(){
-		return this.lastUpdate;
-	}
+    public java.util.Date getLastUpdate() {
+        return this.lastUpdate;
+    }
 
     public CurriculumLecturerBo getCurriculumLecturer() {
         return curriculumLecturer;
--- abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/FollowLecturerService.java
+++ abc12366-bangbang/src/main/java/com/abc12366/bangbang/service/FollowLecturerService.java
@@ -1,5 +1,6 @@
 package com.abc12366.bangbang.service;
 
+import com.abc12366.bangbang.model.FollowLecturer;
 import com.abc12366.bangbang.model.bo.FollowLecturerBO;
 
 import java.util.List;
@@ -15,36 +16,24 @@ import java.util.Map;
 public interface FollowLecturerService {
 
     /**
-     * 新增
-     */
-    FollowLecturerBO insert(FollowLecturerBO followLecturerBO);
-
-    /**
-     * 查询列表
-     */
-    List<FollowLecturerBO> selectList(Map<String, Object> map);
-
-    /**
      * 根据讲师UserId查询所有用户ID
+     *
      * @param userId 讲师UserId
      * @return 所有用户ID
      */
     List<String> selectUserIdListByLecturerId(String userId);
 
     /**
-     * 单个查询
+     * 关注或取消关注讲师
      */
-    FollowLecturerBO selectFollowLecturerBO(String id);
+    boolean followOrUnfollow(FollowLecturer bo);
+
 
     /**
-     * 修改
+     * 查询用户关注讲师列表
+     *
+     * @param map 查询条件
+     * @return 关注列表
      */
-    FollowLecturerBO update(FollowLecturerBO followLecturerBO);
-
-    /**
-     * 删除
-     */
-    void delete(String id, String userId);
-
-    List<FollowLecturerBO> selectBOList(Map<String, Object> map);
+    List<FollowLecturerBO> selectList(Map<String, Object> map);
 }
